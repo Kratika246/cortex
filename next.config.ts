c4a2f68218265dc90852f469b229001c8742af9e
Manav-55 <manav.sharma25800@gmail.com>
2026-04-05 09:36:27 +0530

    corrected

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['groq-sdk'],
-<<<<<<< Updated upstream
   allowedDevOrigins: ['10.11.22.71'],
-=======
->>>>>>> Stashed changes
+
 }
 
 export default nextConfig
